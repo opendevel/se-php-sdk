59ac910a951cd124abeb095762eb1ac00f8b9dba
Robert Šípek <rob.sipek@gmail.com>
2019-04-17 17:19:03 +0200

    Setup nette/tester

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1);
+
+namespace SmartEmailing\Sdk;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Tester\Environment;
+
+\error_reporting(\E_ALL);
+
+
+Environment::setup();
