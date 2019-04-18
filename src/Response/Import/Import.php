a8c4225d36949532bbcf7a449f2be14711fadc41
Robert Šípek <rob.sipek@gmail.com>
2019-04-18 15:07:33 +0200

    Create Response\Import classes

---
+++ src/Response/Import/Import.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+namespace SmartEmailing\Sdk\Response\Import;
+
+use SmartEmailing\Sdk\Collect\Support\Collection;
+use SmartEmailing\Sdk\Response\Import\Model\Contact;
+
+final class Import
+{
+
+    /**
+     * All imported contacts' email addresses mapped to their Ids.
+     * This is only available when not using double opt-in. If double opt-in is used, contacts_map will be empty array.
+     *
+     * @var \SmartEmailing\Sdk\Collect\Support\Collection
+     */
+    private $contacts;
+
+    public function __construct()
+    {
+        $this->contacts = new Collection();
+    }
+
+    public function addContactMap(Contact $contact): void
+    {
+        $this->contacts->add($contact);
+    }
+
+    public function getContacts(): Collection
+    {
+        return $this->contacts;
+    }
+
+    public function newContactMap(array $contactArray): void
+    {
+        if (!isset($contactArray['contact_id']) || is_null($contactArray['contact_id'])) {
+            //@todo exception
+        }
+
+        if (!isset($contactArray['emailaddress']) || is_null($contactArray['emailaddress'])) {
+            //@todo exception
+        }
+
+        $contactArray = new Contact($contactArray['contact_id'], $contactArray['emailaddress']);
+        $this->addContactMap($contactArray);
+    }
+
+}
