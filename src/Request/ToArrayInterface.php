4f97de10ab38a3dafe5c42fb6495153ad2cc9f85
Robert Šípek <rob.sipek@gmail.com>
2019-04-23 04:19:02 +0200

    Create ToArrayInterface class

---
+++ src/Request/ToArrayInterface.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace SmartEmailing\Sdk\Request;
+
+interface ToArrayInterface
+{
+
+    /**
+     * @return mixed[]
+     */
+    public function toArray(): array;
+
+}
