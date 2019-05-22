32dcd3a7c1a3e7572db985c6603c6c3a83028117
Robert Šípek <rob.sipek@gmail.com>
2019-05-22 21:37:17 +0200

    Update Api class

--- src/Api.php
+++ src/Api.php
@@ -4,16 +4,20 @@ namespace SmartEmailing\Sdk\ApiV3Client;
 
 use Http\Message\Authentication;
 use SmartEmailing\Sdk\ApiV3Client\Request\Campaigns\SendCustomEmailsBulkRequest;
+use SmartEmailing\Sdk\ApiV3Client\Request\Campaigns\SendTransactionalEmailsBulkRequest;
 use SmartEmailing\Sdk\ApiV3Client\Request\Contacts\ContactRequest;
 use SmartEmailing\Sdk\ApiV3Client\Request\Contacts\ContactsRequest;
 use SmartEmailing\Sdk\ApiV3Client\Request\Eshops\OrdersRequest;
+use SmartEmailing\Sdk\ApiV3Client\Request\Eshops\ShoppingCartRequest;
 use SmartEmailing\Sdk\ApiV3Client\Request\Import\ImportRequest;
 use SmartEmailing\Sdk\ApiV3Client\Request\Test\CheckCredentials;
 use SmartEmailing\Sdk\ApiV3Client\Request\Test\Ping;
 use SmartEmailing\Sdk\ApiV3Client\Response\Campaigns\SendCustomEmailsBulkResponse;
+use SmartEmailing\Sdk\ApiV3Client\Response\Campaigns\SendTransactionalEmailsBulkResponse;
 use SmartEmailing\Sdk\ApiV3Client\Response\Contacts\ContactResponse;
 use SmartEmailing\Sdk\ApiV3Client\Response\Contacts\ContactsResponse;
 use SmartEmailing\Sdk\ApiV3Client\Response\Eshops\OrdersResponse;
+use SmartEmailing\Sdk\ApiV3Client\Response\Eshops\ShoppingCartResponse;
 use SmartEmailing\Sdk\ApiV3Client\Response\Import\ImportResponse;
 use SmartEmailing\Sdk\ApiV3Client\Response\Test\CheckCredentialsResponse;
 use SmartEmailing\Sdk\ApiV3Client\Response\Test\PingResponse;
@@ -25,52 +29,64 @@ final class Api
      * @var \SmartEmailing\Sdk\ApiV3Client\ApiClient
      */
     private $apiClient;
-    
+
     public function __construct(Authentication $authentication)
     {
         $this->apiClient = new ApiClient($authentication);
     }
-    
+
     public function ping(Ping $apiRequest): PingResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return PingResponse::fromArray(json_decode($result, true));
     }
-    
+
     public function checkCredentials(CheckCredentials $apiRequest): CheckCredentialsResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return CheckCredentialsResponse::fromArray(json_decode($result, true));
     }
-    
-    public function import(ImportRequest $apiRequest): ImportResponse
+
+    public function importContacts(ImportRequest $apiRequest): ImportResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return ImportResponse::fromArray(json_decode($result, true));
     }
-    
-    public function contacts(ContactsRequest $apiRequest): ContactsResponse
+
+    public function getContacts(ContactsRequest $apiRequest): ContactsResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return ContactsResponse::fromArray(json_decode($result, true));
     }
-    
-    public function contact(ContactRequest $apiRequest): ContactResponse
+
+    public function getContact(ContactRequest $apiRequest): ContactResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return ContactResponse::fromArray(json_decode($result, true));
     }
-    
+
     public function sendCustomEmailsBulk(SendCustomEmailsBulkRequest $apiRequest): SendCustomEmailsBulkResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return SendCustomEmailsBulkResponse::fromArray(json_decode($result, true));
     }
-    
-    public function orders(OrdersRequest $apiRequest): OrdersResponse
+
+    public function sendTransactionalEmailsBulk(SendTransactionalEmailsBulkRequest $apiRequest): SendTransactionalEmailsBulkResponse
+    {
+        $result = $this->apiClient->sendRequest($apiRequest);
+        return SendTransactionalEmailsBulkResponse::fromArray(json_decode($result, true));
+    }
+
+    public function addPlacedOrder(OrdersRequest $apiRequest): OrdersResponse
     {
         $result = $this->apiClient->sendRequest($apiRequest);
         return OrdersResponse::fromArray(json_decode($result, true));
+    }
+
+    public function updateShoppingCart(ShoppingCartRequest $apiRequest): ShoppingCartResponse
+    {
+        $result = $this->apiClient->sendRequest($apiRequest);
+        return ShoppingCartResponse::fromArray(json_decode($result, true));
     }
 
 }
